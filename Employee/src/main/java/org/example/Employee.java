f8bc51ef418aef6fe15d914c7f33f922109b6c97
Nkossyk95 <95701026+Nkossyk95@users.noreply.github.com>
2022-06-08 10:36:15 +0200

    Add files via upload

---
+++ Employee/src/main/java/org/example/Employee.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class Employee {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
